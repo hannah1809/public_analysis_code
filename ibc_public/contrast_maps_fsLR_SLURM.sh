791af5c4851d653b5a824cb01725320670ff8445
“hannah”1809 <hannah.01@arcor.de>
2025-10-15 10:48:44 +0200

    slurm adaptation

--- ibc_public/contrast_maps_fsLR_SLURM.sh
+++ ibc_public/contrast_maps_fsLR_SLURM.sh
@@ -10,15 +10,14 @@
 #SBATCH --time=24:00:00
 #SBATCH --partition=compute
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
-## SBATCH --array=0-7 # (subjects-1)
+#SBATCH --array=0-7 # (subjects-1)
 
 export APPTAINER_BIND="/run,/ptmp,/tmp,/opt/ohpc,/home/hmueller2"
 container=/home/rglz/containers/gfae.sif
 
 SUBJECTS_FILE=/ptmp/hmueller2/Downloads/subjects_resting.txt
-# subject=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" $SUBJECTS_FILE)
-
-subject="06"
+subject=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" $SUBJECTS_FILE)
+#subject="06"
 
 echo "---- Starting contrast map calculation for subject $subject ----"
 
