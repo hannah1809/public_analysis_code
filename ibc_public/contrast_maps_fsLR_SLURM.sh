dc3d594ad792ed3ee8937527156ded3b6d9f4df9
“hannah”1809 <hannah.01@arcor.de>
2025-11-04 11:32:31 +0100

    minor

--- ibc_public/contrast_maps_fsLR_SLURM.sh
+++ ibc_public/contrast_maps_fsLR_SLURM.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=contrast_fsLR
-#SBATCH --output=/ptmp/hmueller2/GLM_logs/output/%A_%x.out
-#SBATCH --error=/ptmp/hmueller2/GLM_logs/errors/%A_%x.err
+#SBATCH --output=/ptmp/hmueller2/GLM_logs/output/%A_%x_%a.out
+#SBATCH --error=/ptmp/hmueller2/GLM_logs/errors/%A_%x_%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
@@ -10,19 +10,22 @@
 #SBATCH --time=24:00:00
 #SBATCH --partition=compute
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
-#SBATCH --array=0-7 # (subjects-1)
+# #SBATCH --array=0-7 # (subjects-1)
 
 export APPTAINER_BIND="/run,/ptmp,/tmp,/opt/ohpc,/home/hmueller2"
 container=/home/rglz/containers/gfae.sif
 
-SUBJECTS_FILE=/ptmp/hmueller2/Downloads/subjects_resting.txt
-subject=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" $SUBJECTS_FILE)
-#subject="06"
+#SUBJECTS_FILE=/ptmp/hmueller2/Downloads/subjects_resting.txt
+#subject=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" $SUBJECTS_FILE)
+subject="06"
 
 echo "---- Starting contrast map calculation for subject $subject ----"
+echo "---- Start time: $(date) ----"
 
 srun apptainer exec ${container} \
     bash -c "export PYTHONPATH=/home/hmueller2/ibc_code/ibc_latent/public_analysis:\$PYTHONPATH && python /home/hmueller2/ibc_code/ibc_latent/public_analysis/ibc_public/contrast_maps_fsLR.py $subject"
+
+echo "---- End time: $(date) ----"
 
 exit 0
 
